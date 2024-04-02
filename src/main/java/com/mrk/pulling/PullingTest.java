34a4d69776d183941f8ad66945f6c7cf54ece3c1
BaojunJiang <baojun.jiang@edetek.com>
2024-04-02 13:40:10 +0800

    optimize

--- src/main/java/com/mrk/pulling/PullingTest.java
+++ src/main/java/com/mrk/pulling/PullingTest.java
@@ -4,13 +4,17 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author jiangbaojun
@@ -22,7 +26,8 @@ public class PullingTest {
 
 
     public static void main(String[] args) {
-        String LOCAL_PATH = "D:/persional/wallpaper/";
+//        String LOCAL_PATH = "D:/personal/photo_focus/wallpaper/";
+        String LOCAL_PATH = "C:/Users/BaojunJiang/Desktop/1/";
         if(args!=null && args.length==1){
             LOCAL_PATH = args[0];
         }
@@ -35,11 +40,11 @@ public class PullingTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try {
-            pull360(LOCAL_PATH);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            pull360(LOCAL_PATH);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     /**
@@ -93,26 +98,67 @@ public class PullingTest {
      */
     private static void pullBing(String LOCAL_PATH) throws IOException, ParseException {
         LOCAL_PATH = LOCAL_PATH+"bing/";
+        Map<String,File> existFilesDateMap = getExistFilesDate(LOCAL_PATH);
         String BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160";
         String BING_URL = "https://cn.bing.com";
         String httpContent = HttpUtls.getHttpContent(BING_API);
         JSONObject jsonObject = JSON.parseObject(httpContent);
         JSONArray jsonArray = jsonObject.getJSONArray("images");
-        for (int i = 0; i < jsonArray.size(); i++) {
+        String specialKey = "�";
+        int size = jsonArray.size();
+        for (int i = size-1; i >= 0; i--) {
             jsonObject = (JSONObject)jsonArray.get(i);
             String url = BING_URL + jsonObject.get("url");
             url = url.substring(0, url.indexOf("&"));
-            String startdate = (String)jsonObject.get("startdate");
             String copyright = (String)jsonObject.get("copyright");
-            copyright = copyright.substring(0, copyright.indexOf(" ("));
-            Date date = new SimpleDateFormat("yyyyMMdd").parse(startdate);
+            String name = (String)jsonObject.get("title");
+            String startDate = (String)jsonObject.get("startdate");
+            Date date = new SimpleDateFormat("yyyyMMdd").parse(startDate);
             String dateStr = new SimpleDateFormat("yyyy-MM-dd").format(date);
-            String fileName = LOCAL_PATH+dateStr+"_"+copyright+".jpg";
+            File existFile = existFilesDateMap.get(dateStr);
+            boolean specialName = false;
+            if(copyright!=null && copyright.indexOf(" (")>1){
+                name = copyright.substring(0, copyright.indexOf(" ("));
+            }
+            if(name.contains(specialKey)){
+                specialName = true;
+                name = name.replaceAll(specialKey, "");
+            }
+            if(existFile!=null){
+                if(specialName && !existFile.getName().contains(specialKey)){
+                    //已存在
+                    continue;
+                }else{
+                    existFile.delete();
+                }
+            }
+            String fileName = LOCAL_PATH+dateStr+"_"+name+".jpg";
             System.out.println(fileName);
             File file = new File(fileName);
             if(!file.exists()){
                 FileUtils.copyURLToFile(new URL(url), file);
             }
         }
+    }
+
+    private static Map<String, File> getExistFilesDate(String localPath) {
+        Map<String, File> map = new HashMap<>();
+        File filePath = new File(localPath);
+        if(!filePath.exists()){
+            filePath.mkdirs();
+        }
+        Collection<File> files = FileUtils.listFiles(filePath, new String[]{"jpg", "jpeg"}, false);
+        for (File file : files) {
+            if(file.isDirectory()){
+                continue;
+            }
+            String fileName = file.getName();
+            String[] split = fileName.split("_");
+            String dateStr = split[0];
+            if(dateStr.length()==10){
+                map.put(dateStr, file);
+            }
+        }
+        return map;
     }
 }
